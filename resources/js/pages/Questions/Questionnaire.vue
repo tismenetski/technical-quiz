0e385c3ccfeea48ed68c3791aa7bb02509586d19
tismenetski <tismenetski@gmail.com>
2021-08-04 21:42:30 +0300

    Added Checkboxes

--- resources/js/pages/Questions/Questionnaire.vue
+++ resources/js/pages/Questions/Questionnaire.vue
@@ -15,15 +15,16 @@
 <!--        </el-card>-->
         <div class="question">
             <h3>Question Number {{currentQuestionNumber}}</h3>
-            <p>{{getCurrentQuestion}}</p>
+            <p class="question-text">{{getCurrentQuestion}}</p>
 
             <div v-for="(answer,index) in extractAnswers" :key="index" class="answers">
-                    <div v-if="answer!==null">
-                        <input v type="checkbox" name="index" id="">
+                    <div class="answer" v-if="answer!==null">
+                        <input type="checkbox" :value="answer" v-model="selectedAnswersCurrentQuestion">
                         <span>{{answer}}</span>
                     </div>
             </div>
         </div>
+        <button class="submit-answer">Submit Answers</button>
     </div>
     <div v-else>
         Please Create A New Questionnaire
@@ -40,6 +41,7 @@ export default {
             currentQuestionNumber : 1,
             questionnaireExists : false,
             currentQuestion : null,
+            selectedAnswersCurrentQuestion : [],
 
         }
     },
@@ -70,6 +72,38 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+
+.answer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+.answer span {
+    padding: 10px;
+    font-size: 18px;
+}
+
+.answer input {
+    padding : 10px;
+}
+
+.question-text {
+    font-size: 30px;
+}
+
+.submit-answer {
+    width: 15vh;
+    height: 10vh;
+    background-color: #1f5684;
+    color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    margin: 0 auto;
+    line-height: normal;
+
 }
 
 .box-card {
